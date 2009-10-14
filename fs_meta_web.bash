274fbbcffd0f124536892a014697436b12085f77
danginsburg <danginsburg@2e298075-0c49-4d30-86d1-9432a23a86c5>
2009-10-14 18:14:55 +0000

    Start check in of mri_gui_prototype
    git-svn-id: svn+ssh://natal/home/svn/chb/trunk/scripts@2 2e298075-0c49-4d30-86d1-9432a23a86c5

---
+++ fs_meta_web.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# fs_meta_web.bash
+#
+# Copyright 2009 Dan Ginsburg
+# Children's Hospital Boston
+#
+# GPL v2
+#
+# $1 = Package directory
+# $2 = Scripts directory
+# $3 = Command-line arguments to fs_meta.bash (in quotes)
+#
+# TODO : Clean this up to use Rudolph's common.bash framework
+
+PKGDIR=$1
+FSLDIR=$PKGDIR/fsl
+FSDIR=$PKGDIR/freesurfer
+. $FSLDIR/etc/fslconf/fsl.sh
+. $FSDIR/SetUpFreeSurfer.sh
+PATH=$PATH:$FSLDIR/bin
+PATH=$PATH:$PKGDIR/mricron:$PKGDIR/dtk
+PATH=$PATH:$2
+export FSLDIR PATH
+echo $PATH
+
+fs_meta.bash $3 > /home/danginsburg/fs_meta.bash.std 2> /home/danginsburg/fs_meta.bash.err
