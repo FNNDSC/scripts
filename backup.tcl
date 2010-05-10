5d1165d72033f8df312a9f8dbc0f82f4e75ac9a7
rudolphpienaar <rudolphpienaar@2e298075-0c49-4d30-86d1-9432a23a86c5>
2010-05-10 13:34:01 +0000

    Added the main tcl backup management scripts to 'svn'.
    git-svn-id: svn+ssh://natal/home/svn/chb/trunk/scripts@244 2e298075-0c49-4d30-86d1-9432a23a86c5

---
+++ backup.tcl
@@ -0,0 +1,307 @@
+#!/bin/sh
+# the next line  restart with wish \
+ exec tclsh "$0" $@
+
+set G_SYNOPSIS "
+
+       backup.tcl
+
+SYNOPSIS
+
+       backup.tcl       --user          <user>          \\
+                        --listFileDir   <listFileDir>   \\
+                        --host          <host>          \\
+                        --device        <device>        \\
+                        --label         <label>         \\
+                        --filesys       <filesys>       \\
+                        --currentRule   <currentRule>   \\
+                        --buffer        <buffer>        \\
+                        --rsh           <rsh>           \\
+                        --incReset      <incReset>      \\
+                        --verbose       <on|off>
+
+DESCRIPTION
+
+       `backup.tcl' is the \"thin\" end of the backup_mgr.tcl process. It
+       is typically called only by a controlling backup_mgr program, and
+       receives several command line arguments from this controlling
+       process. These arguments are used to construct a tar command
+       that will ultimately create the archive.
+
+       Note that backup.tcl is machine-specific. The tar command archives
+       *local* file system directories (i.e. no multiple file systems) and
+       does *not* follow cross links.
+
+       The archive creation process is actually part of a pipeline,
+       linking backup.tcl to a `buffer' command via a rsh. This buffer
+       process writes the archive to tape, and consequently is run on the
+       host that contains the backup device (which need not necessarily
+       be the same host on which backup.tcl is run). Obviously it follows
+       that this host needs to be configured to allow backup.tcl remote
+       access.
+
+       Incremental backups are made for weekly and daily runs. The daily
+       backups are referenced to a previous \"weekly\" base; likewise the
+       weekly backups are referenced to a \"monthly\" base run. Note that if
+       for a particular run this base is non existent, a full backup is
+       run and the results used to define a base for subsequent runs.
+
+"
+
+# TODO
+# o        Need to consider the effect of non-monthly archive sets... They are
+#        based on an incremental record, which if non-existant forces a 
+#        complete backup. Perhaps these incremental records need to be 
+#        periodically erased?
+#
+#        Perhaps some rule whereby the *first* daily or weekly backup 
+#        per month erased the previous daily and weekly incremental record.
+# 
+# HISTORY
+#
+# 4-20-1998 / 04-24-1998
+# o        Initial development, testing and debugging.
+#
+# 6-25-1998
+# o        Added -force flag to copy command
+#
+# 6-26-1998
+# o        Tracking down segmentation fault behaviour during monthly
+#        vulcan backups. The tar command will be written to the command 
+#        line and run manually to narrow down the scope of the
+#        error.
+#
+# 6-29-1998
+# o        Changed the construction of the pipeCmd - removed curly brackets
+#        and replaced them with single quotes.
+#
+# 6-30-1998
+# o        Re-implemented the basic archiving mechanism... too many 
+#        difficulties with quoting between tcl grouping commands. Now this 
+#        script creates a child script which contains the archive command 
+#        with necessary quoting. This child is then executed.
+#
+#        Some advantages include cleaner behavior - if the controlling 
+#        `backup_mgr' is killed, the overall backup still proceeds. If the 
+#        child is killed, remote backup stops cleanly with no lingering 
+#        processes hanging about on the target host.
+#
+# 7-2-1998
+# o        Still getting segmentation violation on large archives. I suspect 
+#        that the tcl interpreter dies while trying to catch the *huge* 
+#        output of the tar command.
+#
+#        Two possible solutions:-
+#                o Redirect all output to a file, which is parsed later
+#                o Remove the `verbose' flag from the tar command
+#
+#        Simpler is probably better. Verbose is removed.
+#
+# 7-13-1998
+# o        Added additional command line parameter, incReset, that
+#        controls whether or not to erase the incremental data base used
+#        by `tar'. For backup sets that do not have a monthly specifier, 
+#        periodic cycling of the incremental file need to be effected - this
+#        is specified with the incReset parameter with `yes' argument.
+#
+#        Note that the decision whether or not to implement this reset is
+#        made by the managing `backup_mgr.tcl' process.
+#        
+# 7-22-1998
+# o        Added archive set name to incremental title.
+#
+# 7-28-1998
+# o        Added -v on flag for verbose logging
+#
+# 01-11-2000
+# o        Re-evaluation
+# o        Beautify
+#
+# 01-13-2000
+# o        Command line arguments need two dashes ("--")!
+#
+# 01-14-2000
+# o        Changed command line arguments to new GNU style
+# o        Forced `verbose' on all archives
+#
+# 02 December 2000
+# o        Added -force to all file delete references
+#
+
+lappend auto_path       ~/src/devel/tcl_packages.devel
+package require         misc
+package require         parval
+
+###\\\
+# Globals --->
+###///
+
+set SELF                "backup.tcl"
+set tarcmd              ""
+set modFileSys          ""
+set state               flag
+set listedIncremental   ""
+set lst_commargs {
+    user host device label filesys currentRule buffer rsh incReset verbose listFileDir
+}
+foreach var $lst_commargs {set $var ""}
+
+###\\\
+# Function definitions --->
+###///
+
+proc synopsis_show {} {
+    global SELF G_SYNOPSIS
+
+    puts "$G_SYNOPSIS"
+
+}
+
+proc error_exit {action errorMessage code} {
+#
+# ARGS
+# action                in                action being attempted
+# message               in                message text
+# code                  in                system exit code
+#
+# DESC
+# Simple display of error message and exit
+#
+    global SELF
+
+    puts stderr "\n$SELF:\n\tSorry, but there seems to be an error."
+    puts stderr "\tWhile $action,"                                     
+    puts stderr "\t$errorMessage\n"                                           
+    synopsis_show
+    puts stderr "\n\n\t -- Exiting with code $code -- \n"
+    exit $code
+}
+
+###\\\
+# main --->
+###///
+
+# Parse command line arguments, defining internal script variables
+
+set arr_PARVAL(0) 0
+PARVAL_build commswitch $argv "--" "1"
+foreach element $lst_commargs {
+    PARVAL_interpret commswitch $element
+    if { $arr_PARVAL(commswitch,argnum) >= 0 } {
+        set $element $arr_PARVAL(commswitch,value)
+    }
+}
+
+# Check that all variables have assigned values
+foreach var $lst_commargs {
+    if {[set $var]==""} {
+        set action "parsing command line arguments"
+        set message "Not all required variables are set!"
+        append message "\n\n\tPerhaps I have been called with some flags missing?"
+        append message "\n\tI couldn't find a value for `$var'."
+        error_exit $action $message 1
+    }
+}
+
+append listFileDir "-"
+append listFileDir [exec hostname]
+if {![file isdirectory $listFileDir]} {file mkdir $listFileDir}
+set child                        "${listFileDir}/archive.sh"
+
+# Build the tar command
+switch -- [exec uname] {
+    Linux               { append tarcmd "/bin/tar " }
+    FreeBSD             { append tarcmd "/usr/bin/tar " }
+    Darwin              { append tarcmd "/sw/bin/tar " }
+    default             { append tarcmd "/bin/tar " }
+}
+append tarcmd "--create --file - --totals --gzip "
+#append tarcmd "--block-size 80 "
+append tarcmd "--label "
+append tarcmd "\"$label\" "
+append tarcmd "--verbose "
+#if {$verbose=="on"} {
+#    append tarcmd "--verbose "
+#}
+
+# Define the incremental reference filename
+set fullhostname                [exec hostname]
+set hostname                    [split $host "."]
+set host                        [lindex $hostname 0]
+set forwardToBack               [exec echo $filesys | sed "s./.:.g"]
+set modFileSys                  [lindex [split $label :] 0]
+append modFileSys               "::$host:"
+append modFileSys               $forwardToBack
+set listedIncremental           ${listFileDir}/$modFileSys
+
+if {$currentRule=="monthly"} {
+    catch {[file delete -force "$listedIncremental-*"]} result
+    append tarcmd "--listed-incremental \"$listedIncremental-monthly\" "
+    file delete -force $listedIncremental-monthly
+} else {
+    if {$incReset == "yes"} {
+        catch {[file delete -force "$listedIncremental-*"]} result
+    }
+    switch -- $currentRule {
+        weekly         {set base monthly}
+        daily         {set base weekly}
+    }
+    append tarcmd "--listed-incremental \"${listedIncremental}-${base}\""
+}
+
+append tarcmd " \"$filesys\""
+
+# Check on the $device variable. If this does not contain "/dev"
+#        then assume we are backing up to a hard drive. Change the
+#        $device variable to reflect this.
+set path        [split $device "/"]
+set dev         [lindex $path 1]
+
+if {$dev != "dev"} {
+    set date            [exec date]
+    set today           [lindex $date 0]
+    set month           [lindex $date 1]
+    set day             [lindex $date 2]
+    set hour            [lindex $date 3]
+    set year            [lindex $date 5]
+    regsub -all ":" $label "_" label2
+    regsub -all "/" $label2 "." label3
+    append device       "/${label3}.${currentRule}.${today}.tgz"
+}
+
+# Build the child shell script that will do the actual backup
+set childID [open $child w 0755]
+puts $childID "#!/bin/sh"
+puts $childID ""
+puts $childID "# This script is automatically created by $SELF"
+puts $childID "# for backup processing"
+puts $childID ""
+puts $childID "# Creation date [exec date]"
+puts $childID "# DO NOT EDIT!"
+puts $childID ""
+puts $childID "$tarcmd | $rsh -l $user $host \'$buffer > \"$device\"\'" 
+close $childID
+
+# Now execute the child shell script
+# The actual files that were archived (stderr) are directed to
+# to ${child}.result. The directory tree is captured in dirtree
+set err [catch {exec $child 2> $listedIncremental.results} dirtree]
+
+# Some error checking
+if {$err} {
+    set action "executing child tar process"
+    set errorMessage "An unknown error ($dirtree) occured in the backup!"
+    append errorMessage "\n\tEither the `tar' or remote `buffer' process failed."
+    file delete -force "$listedIncremental-*"
+    error_exit $action $errorMessage 1
+}
+
+if {$currentRule=="monthly"} { 
+    file copy -force $listedIncremental-monthly $listedIncremental-weekly
+}
+
+# Parse results file for bytes written
+
+set ok [catch {exec grep bytes $listedIncremental.results} bytes] 
+puts stdout $bytes
+exit 0
---
+++ backup_mgr.tcl
@@ -0,0 +1,368 @@
+#!/bin/sh 
+# the next line  restart with wish \
+ exec tclsh "$0" $@
+
+set G_SYNOPSIS "
+
+NAME
+
+      backup_mgr.tcl
+
+SYNOPSIS
+
+      backup_mgr.tcl          \[--archive <archive_set>]                \\
+                              \[--backupRootDir <backupRootDir>]        \\
+                              \[--tapeInit <option_init_tape_command>]  \\
+                              \[--rule <force_rule>]                    \\
+                              \[--init <self|filesystem>]               \\
+                              \[--day <forceDay>]                       \\
+                              \[--Reset]                                \\
+                              \[--usage]
+
+DESCRIPTION
+
+        `backup_mgr.tcl' is the main backup manager. It creates and
+        maintains the backup archive objects, schedules the backup
+        types, stores state information to disk, etc.
+
+        The command line arguments are almost always used only for
+        debugging/testing and allow the calling process (or user)
+        to override backup_mgr's default behaviour.
+
+ARGS
+
+    o --archive <archive_set>
+            This can force backup_mgr to concentrate on a
+            specific archive, and ignore whatever it initialized
+            itself with. Note that at this stage, only ONE
+            archive_set name will be interpreted.
+    o --backupRootDir <backupRootDir>
+             This specifies the root directory containing the 
+             backup data folder, backup_lists, and object
+             configuration files.
+    o --tapeInit <option_init_tape_command>
+             Any optional tape commands can be specified. These
+             will be executed *before* any of the backups begin.
+             Again, note that the only real command that I had
+             in mind was `rewind'. Complex init sequences with
+             white space won't work!
+    o --rule <force_rule>
+             Force backup_mgr to use rule <force_rule> irrespective
+             of what should have been scheduled.
+    o --day <forceDay>
+             Force backup_mgr to use the ruleset for day <forceDay>.
+    o --init <self|filesystem>
+            Initialize tape objects (rules, priorities, hosts, etc)
+            either from a stored state on disk, or recreate from
+            scratch (usually encoded within this script).
+    o --Reset
+            Similar to above, with implicit self initialization.
+            Once setup, processing will terminate without performing
+            the backup.
+    o --usage
+            This synopsis.
+#
+"
+# TODO
+#
+# (Short term)
+# o        Ability to pass additional parameters to tar command
+# o        More error checking
+# o        State and infrastructure testing
+# o        Separate configuration information
+#
+# o        Check `archiveDate'
+# o        Check 'status'
+#
+# o        `nop' override in class - for overriding a scheduled cron
+#        job without needing to acutally remove it.
+#
+# (Long term)
+# o        Higher dimension array construction (i.e. > 2)
+# o        Socket communication between different processes for status 
+#        information?
+# o        Ordering of tape archives in order of daily, weekly, monthly - 
+#        thus if a daily is scheduled on the same day using the same 
+#        device as the weekly/monthly, the daily will backup first, then 
+#        the weekly will rewind and backup, and then the monthly will 
+#        rewind and backup. Basically just to place preference on 
+#        collision days.
+#
+# HISTORY
+#
+# End April 1998
+# o        Initial design, testing, debugging
+# o        Split into separate module components
+#
+# 5-11-1998
+# o        Added command line interpretation -
+#        allows for process to be run on specific tape set
+#        with a given rule. Note that rule is forced on each
+#        element of command line set.
+#
+# 5-14-1998
+# o        General cron-related problems... cron launched apps do not
+#        have shell variables set - i.e. no path!
+#
+# 5-15-1998
+# o        Added `totalBackups' parameter to class definition. This keeps
+#        track of the total number of backups per set, allowing subsequent
+#        backups to fsf appropriately
+#
+# 6-25-1998
+# o        Crontab behaviour is still not optimal. Eventhough the process is 
+#        called in cron from the target directory, i.e. 
+#        /root/backup/backup_mgr.tcl, the process dumps its working files 
+#        to /root/backup. Apparently cron runs scheduled tasks from the 
+#        owner's home directory.
+#        
+#        The best solution is to specify working directory and path 
+#        in backup_mgr.tcl
+#
+# o        Working directory data path added to basic class definition
+#
+# 7-6-1998
+# o        Sorted archive list. Lower priority archives are scheduled first, 
+#        with important archives last. Priority is defined as the current 
+#        rule: `none' has least priority, with `daily' more priority 
+#        scaling all the way to `monthly' with the most. This addresses 
+#        the problem where on a particular day one archive might have a 
+#        `daily' backup scheduled, and another a `monthly'. If both 
+#        archives use the same remote device, i.e. device clash, the lower 
+#        priority will occur first, and then when the higher priority 
+#        backup runs, it will in effect overwrite the earlier process's 
+#        backup.
+#
+# 7-21-1998
+# o        Added `date' value to class structure
+#
+# 1-10-2000
+# o        Resurrected and began integration into new env.
+#        Initial focus is on getting existing code up and running again
+#        without worrying about possible redesign.
+#
+# 1-15-2000
+# o     Testing full run!
+#
+# 1-20-2000
+# o        Changed tape device back to nst
+#
+# 22 September 2004
+# o        Resurrected! For the second time.
+#        Made changes to reflect mgh environment
+#
+# January 2010
+# o     CHB deployment
+# o     Spec for backupRootDir
+# 
+
+
+###\\\
+# NB NB
+# The following need to be defined for a given backup system!!
+###///
+# The path containing the support tcl files.
+lappend auto_path       ~/src/devel/tcl_packages.devel
+# The <dataPath> is the backupRootDir and contains the config files for
+# a backup, as well as the actual backup tgz files for each config.
+set dataPath            "/chb/users/rudolphpienaar/backup"
+# The <lst_archive> contains the names (filenames) of each backup archive
+set lst_archive                 {fnndsc-etc fnndsc-localUsers fnndsc-chb}
+
+###\\\
+###|||
+###||| You shouldn't need to change anything below here!
+###|||
+###///
+
+
+###\\\
+# include --->
+###///
+package require         class_struct        
+package require         misc                
+package require         tape_misc        
+package require         parval                
+
+###\\\
+# globals --->
+###///
+
+set SELF                "backup_mgr.tcl"
+set ext                 "object"
+set lst_base_struct     [tape_class_struct]
+set delim               ">"
+
+# Actions
+set AM_remoteDevice     "communicating with remote device"
+set AM_pingHost         "pinging remote host"
+set AM_rsh              "attempting to rsh"
+set AM_parseResults     "parsing results file"
+set AM_parseStatus      "parsing status file"
+set AM_backup           "performing the backup"
+
+# Error messages
+set EM_remoteDevice     "I could not contact the remote device"
+set EM_pingHost         "I could not ping host"
+set EM_rsh              "I could not seem to remotely log in"
+set EM_parseResults     "I could not parse the results of the backup"
+set EM_parseStatus      "I could not parse the status of the backup"
+set EM_backup           "The backup seems to have failed"
+
+# Error codes
+set EC_remoteDevice     1
+set EC_pingHost         2
+set EC_rsh              3
+set EC_parseResults     4
+set EC_parseStatus      5
+set EC_backup           6
+
+# Week day index
+set lst_weekdays        [weekdays_list]
+
+#
+# Types of backup operation (the order in action lists is important!)
+#
+#        o `monthly'            full (non incremental) backup
+#        o `weekly'             weekly incremental
+#                               - referenced to most recent monthly
+#        o `daily'              daily  incremental
+#                               - referenced to most recent weekly
+#
+set lst_actions                {monthly weekly daily none}
+set lst_actionsPriority        {none daily weekly monthly}
+
+#
+# The actual archives.
+#        o Each archive *must* have a set of rules (defined below)
+#
+set lst_archive                 {fnndsc-etc fnndsc-localUsers fnndsc-chb}
+
+
+###\\\
+# Function definitions --->
+###///
+
+proc synopsis_show {} {
+    global SELF G_SYNOPSIS
+    set base "none"
+
+    puts "$G_SYNOPSIS"
+
+    shutdown 0
+}
+
+proc shutdown {exitcode} {
+#
+# exitcode        in        code returned to system
+#
+# process shut down and exit
+#
+    global SELF
+
+    puts "\n`$SELF' shutting down..."
+    puts "\tSending system exitcode of $exitcode\n"
+    exit $exitcode
+}
+
+###\\\
+# main --->
+###/// 
+set date                [exec date]
+set today               [lindex $date 0]
+set todayDate           [lindex $date 2]
+
+set day                 $today
+set archive             "void"
+set rule                "void"
+set init                "filesystem"
+set tapeInit            "void"
+set Reset               0
+set usage               0
+
+set lst_commargs        {archive rule init tapeInit usage Reset day backupRootDir}
+set arr_PARVAL(0) 0
+PARVAL_build commswitch $argv "--"
+foreach element $lst_commargs {
+    PARVAL_interpret commswitch $element
+    if { $arr_PARVAL(commswitch,argnum) >= 0 } {
+        set $element $arr_PARVAL(commswitch,value)
+    }
+}
+
+if ![catch {set backupRootDir}] {
+  set dataPath $backupRootDir
+} 
+
+if {$usage} {synopsis_show}
+
+if {$archive != "void" } {
+    set lst_archive $archive
+}
+
+if {$init == "self" || $Reset} {
+    foreach tape $lst_archive {
+        class_Initialise arr_${tape}_currentSet lst_actions lst_${tape}_currentSet
+        class_Initialise arr_${tape}_totalSet   lst_actions lst_${tape}_totalSet
+        class_Initialise arr_${tape}_rules      lst_weekdays lst_rules_$tape
+        # Initialise the class according to the class definition structure
+        class_Initialise arr_${tape} lst_base_struct    \ 
+                "$tape                                  \ 
+                {$date}                                 \
+                $dataPath                               \
+                arr_${tape}_currentSet                  \ 
+                arr_${tape}_totalSet                    \
+                arr_${tape}_rules                       \
+                none                                    \ 
+                $remoteHost                             \
+                $remoteUser                             \ 
+                $remoteDevice                           \
+                $rsh $adminUser                         \
+                {$notifyTape}                           \
+                {$notifyTar}                            \
+                {$notifyError}                          \
+                ptr_${tape}_partition                   \
+                {}                                      \
+                {} "
+        class_Dump arr_${tape} ${dataPath}/${tape}.${ext}
+    }
+} else {
+    foreach tape $lst_archive {
+        class_Initialise arr_${tape} lst_base_struct {} ${dataPath}/${tape}.${ext}
+    }
+}
+
+if {$Reset} {shutdown 0}
+
+# Sort the archives according to priority
+foreach action $lst_actionsPriority {
+    foreach tape $lst_archive {
+        if {[tape_todayRule_get arr_$tape $day] == "$action"} {
+            lappend lst_sorted_archive $tape
+        }
+    }
+}
+
+puts "$SELF"
+puts "\tUnordered list of archives:\t$lst_archive"
+puts "\tProcessing archives in order:\t$lst_sorted_archive\n"
+
+#foreach today $lst_weekdays {
+#    puts ""
+    foreach tape $lst_sorted_archive {
+        set ok [tape_backup_manage arr_${tape} $rule $day $tapeInit]
+        if {$ok} {
+            puts stdout "Status ok. Dumping $tape object to file."
+            class_Dump arr_${tape} ${dataPath}/${tape}.${ext}
+        }
+    }
+#}
+
+if {$ok == 1}  {
+    shutdown 0
+} else {
+    puts "WARNING"
+    puts "\tSome internal error was sent back to main controlling process."
+    shutdown 1
+}
+
