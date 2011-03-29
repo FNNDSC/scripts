50ae4eccb442a47fd0cb78099a02f21693100674
dicom <dicom@2e298075-0c49-4d30-86d1-9432a23a86c5>
2011-03-29 16:00:37 +0000

    Add nibabel dev because it is needed to workaround a bug with 'w2' error.
    git-svn-id: svn+ssh://natal/home/svn/chb/trunk/scripts@413 2e298075-0c49-4d30-86d1-9432a23a86c5

--- pl_batch_web.bash
+++ pl_batch_web.bash
@@ -21,7 +21,7 @@ PATH=$PATH:$PKGDIR/mricron:$PKGDIR/dtk:$PKGDIR/gdcm/bin:$PKGDIR/Slicer/current
 PATH=$PATH:$2
 export PATH
 echo $PATH
-PYTHONPATH=$PYTHONPATH:/chb/arch/python/cmp:/chb/arch/python/cfflib
+PYTHONPATH=$PYTHONPATH:/chb/arch/python/cmp:/chb/arch/python/cfflib:/chb/arch/python/nibabel
 export PYTHONPATH
 cd $4
 pl_batch.bash $3 > $5.std 2> $5.err
