26801729da1a60679ce986ca7aa3e8b122200679
rudolphpienaar <rudolphpienaar@2e298075-0c49-4d30-86d1-9432a23a86c5>
2011-04-07 15:32:51 +0000

    Simply scripts that mount Siemens OC consoles' HDD.
    git-svn-id: svn+ssh://natal/home/svn/chb/trunk/scripts@417 2e298075-0c49-4d30-86d1-9432a23a86c5

---
+++ mount_longwood-3.0T.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo smbmount //10.3.1.214/MedCom /mnt/siemens_oc_longwood-3.0T -o uid=6244,gid=1102,user=meduser,password=meduser1
---
+++ mount_waltham-3.0T.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo smbmount //10.64.4.241/Disk_C /mnt/siemens_oc_waltham-3.0T -o uid=6244,gid=1102,user=meduser,password=meduser1
