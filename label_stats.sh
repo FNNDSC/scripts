b43d9aa1a415e21fd49b113362466c1b127e81e5
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2011-11-16 16:47:46 -0500

    Fix another path issue with label_stats.sh.

--- label_stats.sh
+++ label_stats.sh
@@ -1,1 +1,2 @@
-/chb/arch/Linux64/packages/Slicer4/current/Slicer-build/Slicer --python-script lib/Slicer-4.0/qt-scripted-modules/LabelStatistics.py --no-main-window
+SLICER4HOME=/chb/arch/Linux64/packages/Slicer4/current/Slicer-build
+$SLICER4HOME/Slicer --python-script $SLICER4HOME/lib/Slicer-4.0/qt-scripted-modules/LabelStatistics.py --no-main-window
