8fe6cefc938b93271da71d65c5d57dcf2f4f8dc4
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2011-11-16 16:44:36 -0500

    Fix typo in label_stats.sh.

--- label_stats.sh
+++ label_stats.sh
@@ -1,1 +1,1 @@
-/chb/arch/Linux64/Slicer4/current/Slicer-build/Slicer --python-script lib/Slicer-4.0/qt-scripted-modules/LabelStatistics.py --no-main-window
+/chb/arch/Linux64/packages/Slicer4/current/Slicer-build/Slicer --python-script lib/Slicer-4.0/qt-scripted-modules/LabelStatistics.py --no-main-window
