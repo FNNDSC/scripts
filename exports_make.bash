fd45d83c42edf146dcdca795e26136f1bdeeb155
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2011-12-13 08:50:45 -0500

    Added another wireless network to 1 AU descriptors.

--- exports_make.bash
+++ exports_make.bash
@@ -65,7 +65,7 @@ done
 G_STYLE=$(string_clean $G_STYLE)
 if [[ $G_STYLE != "Linux" && $G_STYLE != "Darwin" ]] ; then fatal args;   fi
 
-sitenum=13
+sitenum=14
 
 # Format: <label>;<netmask>
 NETMASK[0]="1 Autumn Street, 6th floor;10.17.24.0"
@@ -78,9 +78,10 @@ NETMASK[6]="Waltham (WL13W3 - subnet 1);10.64.4.0"
 NETMASK[7]="Waltham (WL13W3 - subnet 2);10.64.5.0"
 NETMASK[8]="Waltham (WL13W3 - subnet 3);10.64.84.0"
 NETMASK[9]="1 Autumn Street, TCHpeap subnet 1;10.23.50.0"
-NETMASK[10]="1 Autumn Street, TCHpeap subnet 2;10.23.130.0"
-NETMASK[11]="1 Autumn Street, TCHpeap subnet 3;10.23.129.0"
-NETMASK[12]="Main CHB Campus, 3D Lab;10.3.2.0"
+NETMASK[10]="1 Autumn Street, TCHpeap subnet 3;10.23.129.0"
+NETMASK[11]="1 Autumn Street, TCHpeap subnet 2;10.23.130.0"
+NETMASK[12]="1 Autumn Street, TCHpeap subnet 4;10.23.133.0"
+NETMASK[13]="Main CHB Campus, 3D Lab;10.3.2.0"
 
 shift $(($OPTIND - 1))
 EXPORTLIST=$*
