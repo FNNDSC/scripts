b895eeaa494cee2b1a57cf22bb95a122eecaf56f
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2011-12-21 07:53:00 -0500

    Added b10_convertFrom and permutations_find/neighbours_find

--- systemMisc.py
+++ systemMisc.py
@@ -44,7 +44,10 @@ def array2DIndices_enumerate(arr):
                                           element the rows and the second
                                           the cols
         
+        RET
+            A list of explicit 2D coordinates.
         """
+        
         rows            = arr[0]
         cols            = arr[1]
         arr_index       = zeros( (rows*cols, 2) )
@@ -59,7 +62,7 @@ def error_exit(         astr_func,
                         astr_action,
                         astr_error,
                         aexitCode):
-        print "%s: FATAL ERROR"
+        print "FATAL ERROR"
         print "\tSorry, some error seems to have occurred in <%s::%s>" \
                 % ('systemMisc', astr_func)
         print "\tWhile %s"                                  % astr_action
@@ -83,6 +86,242 @@ def list_removeDuplicates(alist):
 
     alist = list(set(alist))
     return alist
+
+def b10_convertFrom(anum10, aradix, *args):
+    """
+        ARGS
+        anum10            in      number in base 10
+        aradix            in      convert <anum10> to number in base
+                                  + <aradix>
+        
+        OPTIONAL
+        forcelength       in      if nonzero, indicates the length
+                                  + of the return array. Useful if
+                                  + array needs to be zero padded.
+        
+        DESC
+        Converts a scalar from base 10 to base radix. Return
+        an array.
+        
+        NOTE:
+        "Translated" from a MatLAB script of the same name.
+    """
+    i = 0;
+    k = 0;
+    
+    # Cycle up in powers of radix until the largest exponent is found.
+    # This is required to determine the word length
+    while (pow(aradix,i)) <= anum10:
+        i = i+1;
+    forcelength = i
+
+    # Optionally, allow user to specify word length
+    if len(args): forcelength = args[0]
+
+    # Check that word length is valid
+    if(forcelength and (forcelength < i)):
+        error_exit('b10_convertFrom',                    
+                   'checking on requested return array', 
+                   'specified length is too small',
+                   1)
+
+    numm = anum10;
+    num_r = zeros((1, forcelength));
+    if(i):
+        k = forcelength - i;
+    else:
+        k = forcelength - 1;
+
+    if(anum10==1):
+        num_r[(0,k)] = 1;
+        return num_r;
+
+    for j in arange(i,0,-1):
+        num_r[(0,k)] = fix(numm / pow(aradix,(j-1)));
+        numm = numm % pow(aradix, (j-1));
+        k = k+1;
+    return num_r
+
+def permutations_find(a_dimension, a_depth, *args):
+    """
+    
+         SYNOPSIS
+         
+              [I, D] = permutations_find(a_dimension, a_depth, *args)
+        
+         ARGS
+         
+           INPUT
+           a_dimension         int32           number of dimensions
+           a_depth             int32           depth of neighbours to find
+        
+           OPTIONAL
+           av_row              array           row vector that is optionally
+                                               added to each I and D row. This
+                                               is useful for expressing a 
+                                               permutation in terms of an
+                                               actual offset baseline vector.
+           
+           OUTPUT
+           I                   cell           a cell array containing 
+                                                  "indirect" neighbour 
+                                                   information. Each index
+                                                   of the cell array is a row - order
+                                                   matrix of "index" distant
+                                                   indirect neighbours
+           D                   cell           a cell array containing 
+                                                   "direct" neighbour 
+                                                   information. Each index
+                                                   of the cell array is a row - order
+                                                   matrix of "index" distant
+                                                   direct neighbours
+        
+         DESC
+               This method determines the neighbours of a point in an
+               n - dimensional discrete space. The "depth" (or ply) to
+               calculate is `a_depth'.
+        
+               Indirect neighbours are non-orthogonormal.
+               Direct neighbours are orthonormal.
+               
+               This operation is identical to finding all the permutations
+               of a given set of elements.
+        
+        
+         PRECONDITIONS
+               o The underlying problem is discrete.
+        
+         POSTCONDITIONS
+               o I cell array contains the Indirect neighbours
+               o D cell array contains the Direct neighbours
+               o size{I} = size{D}
+               o size{I} = a_depth
+        
+         HISTORY
+         20 December 2011
+         o Translated from MatLAB script of same name.
+         
+    """
+    if (a_depth<1):
+        D       = None;
+        I       = None;
+        l_D     = []
+        l_I     = {}
+        return I, D;
+
+    # Allocate space for neighbours structure
+    D = zeros( (1, a_depth), dtype='object');
+    I = zeros( (1, a_depth), dtype='object');
+    l_D = [0] * a_depth
+    l_I = [0] * a_depth
+
+    # Pre-allocate internal data structures
+    v_rowOffset     = zeros( (1, a_dimension) );
+    b_rowOffset     = 0;
+    if len(args):
+        av_rowOffset = args[0];
+        rows, cols   = av_rowOffset.shape
+        if rows == 1 and size(av_rowOffset)==a_dimension:
+            v_rowOffset     = av_rowOffset;
+            b_rowOffset     = 1;
+
+    d                   = 1;
+    hypercube           = pow((2*d+1), a_dimension);
+    hypercubeInner      = 1;
+    orthogonals         = 2*a_dimension;
+    l_D[0]              = zeros( (orthogonals, a_dimension) )
+    l_I[0]              = zeros( (hypercube - orthogonals -1, a_dimension) );
+    D[0,0]              = zeros( (orthogonals, a_dimension) );
+    I[0,0]              = zeros( (hypercube - orthogonals -1, a_dimension) );
+    for d in arange(1,a_depth):
+        hypercubeInner  = hypercube;
+        hypercube       = pow(2*d+1,a_dimension);
+        l_D[d]          = zeros( (orthogonals, a_dimension) )
+        l_I[d]          = zeros( (hypercube - orthogonals - hypercubeInner))
+        D[0, d]          = zeros( (orthogonals, a_dimension) );
+        I[0, d]          = zeros( (hypercube - orthogonals -
+                                   hypercubeInner, a_dimension));
+    # Offset and "current" vector
+    M_bDoffset      = ones( (1, a_dimension) ) * -a_depth;
+    M_current       = ones( (1, a_dimension) );
+    M_currentAbs    = ones( (1, a_dimension) );
+
+    # Index counters
+    M_ii            = zeros( (1, a_depth) );
+    M_dd            = zeros( (1, a_depth) );
+
+    # Now we loop through *each* element of the last hypercube
+    # and assign it to the appropriate matrix in the I,D structures
+    for i in arange(0,hypercube):
+        str_progress    = 'iteration %5d (of %5d) %3.2f' % \
+                                (i, hypercube-1, i/(hypercube-1)*100);
+        M_current       = b10_convertFrom(i, (2*a_depth+1), a_dimension);
+        M_current       = M_current + M_bDoffset;
+        M_currentAbs    = abs(M_current);
+        neighbour       = max(max(M_currentAbs));
+        if(sum(M_currentAbs) > neighbour):
+            l_I[int(neighbour-1)][M_ii[0, neighbour-1]]   = M_current
+            I[0,neighbour-1][M_ii[0, neighbour-1]]        = M_current;
+            M_ii[0, neighbour-1] += 1;
+        else: 
+            if(sum(M_currentAbs) == neighbour and neighbour):
+                l_D[int(neighbour-1)][M_dd[0, neighbour-1]]     = M_current
+                D[0, neighbour-1][M_dd[0, neighbour-1]]         = M_current;
+                M_dd[0, neighbour-1] += 1;
+
+    if b_rowOffset:
+        for layer in arange(0, neighbour):
+            rowsI, colsI   = I[layer].shape;
+            rowsD, colsD   = D[layer].shape;
+            M_OI           = tile(v_rowOffset, (rowsI, 1));
+            M_OD           = tile(v_rowOffset, (rowsD, 1));
+            I[0, layer]    = I[0, layer] + M_OI;
+            D[0, layer]    = D[0, layer] + M_OD;
+    return l_I, l_D
+
+
+def base10toN(num, n):
+    """Change a num to a base-n number.
+    Up to base-36 is supported without special notation."""
+    num_rep={10:'a',
+         11:'b',
+         12:'c',
+         13:'d',
+         14:'e',
+         15:'f',
+         16:'g',
+         17:'h',
+         18:'i',
+         19:'j',
+         20:'k',
+         21:'l',
+         22:'m',
+         23:'n',
+         24:'o',
+         25:'p',
+         26:'q',
+         27:'r',
+         28:'s',
+         29:'t',
+         30:'u',
+         31:'v',
+         32:'w',
+         33:'x',
+         34:'y',
+         35:'z'}
+    new_num_string=''
+    current=num
+    while current!=0:
+        remainder=current%n
+        if 36>remainder>9:
+            remainder_string=num_rep[remainder]
+        elif remainder>=36:
+            remainder_string='('+str(remainder)+')'
+        else:
+            remainder_string=str(remainder)
+        new_num_string=remainder_string+new_num_string
+        current=current/n
+    return new_num_string
 
 def list_i2str(ilist):
     """
