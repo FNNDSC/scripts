b2caa08a876fd5d49015e0cd91783eb824c4ffb4
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-01-19 11:47:23 -0500

    Fix path to 3D Slicer 4.

--- label_stats.sh
+++ label_stats.sh
@@ -1,2 +1,2 @@
-SLICER4HOME=/chb/arch/Linux64/packages/Slicer4/current/Slicer-build
+SLICER4HOME=/chb/arch/Linux64/packages/Slicer4/current/
 $SLICER4HOME/Slicer --python-script $SLICER4HOME/lib/Slicer-4.0/qt-scripted-modules/LabelStatistics.py --no-main-window --disable-cli-modules
