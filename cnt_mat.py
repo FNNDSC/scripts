34ce8e77b4839c4aea616d5ac1a4d4097af84574
Nicolas Rannou <huko.rannou@gmail.com>
2012-05-02 13:47:31 -0400

    old cnt_mat.py moved to https://github.com/FNNDSC/deprecated rename cnt_mat-next.py to cnt_mat.py

--- cnt_mat.py
+++ cnt_mat.py
@@ -5,38 +5,76 @@ import networkx as nx
 import numpy as np
 import scipy.io as sio
 
+import argparse
 
-pickleFileName = sys.argv[1];
-outputBaseName = sys.argv[2];
+#----------------
+# Parse arguments
 
-print ("Loading %s...\n") % (pickleFileName)
-g = nx.read_gpickle(pickleFileName)
+parser = argparse.ArgumentParser( description='The connectome matrix tool: Generating matrices from pickles')
+
+parser.add_argument( '-p', '--pickle',
+                    action='store',
+                    dest='pickle',
+                    required=True,
+                    help='Target pickle file.' )
+
+parser.add_argument( '-b', '--base',
+                    action='store',
+                    dest='base',
+                    default='',
+                    help='Output basename for the files to be generated. A "test" base would generate test_***.extension files.' )
+
+parser.add_argument( '-e', '--extension',
+                    action='store',
+                    dest='extension',
+                    default='txt',
+                    choices=['txt', 'mat'],
+                    help='Output extension for the files to be generated. A "txt" extension would generate base_***.txt files.' )
+
+options = parser.parse_args()
+
+#----------------
+# setup parameters
+
+print ('Setup parameters...')
+
+listkeys = ['number_of_fibers', 'fiber_length_mean', 'fiber_length_std']
+listformat  = ['%d', '%f', '%f']
     
-# Write out three different matrices, each with different edge values
-mlab_n = np.zeros([len(g.nodes())+1,len(g.nodes())+1])
-mlab_lm = np.zeros([len(g.nodes())+1,len(g.nodes())+1])
-mlab_ls = np.zeros([len(g.nodes())+1,len(g.nodes())+1])
+#----------------
+# Load pickle and generate matrices
+
+print ('Load %s and generate matrices...') % (options.pickle)
+
+g = nx.read_gpickle(options.pickle)
+
+# Write out different matrices, each with different edge values
+for mat in listkeys:
+  exec(mat +' = np.zeros([len(g.nodes())+1,len(g.nodes())+1])')
 
 for i in g.edge.keys():
     for j in g.edge[i].keys():
-        mlab_n[i-1][j-1]=g.edge[i][j]['number_of_fibers']
-        mlab_lm[i-1][j-1]=g.edge[i][j]['fiber_length_mean']
-        mlab_ls[i-1][j-1]=g.edge[i][j]['fiber_length_std']
+        for mat in listkeys:
+          exec(mat +'[i-1][j-1]=g.edge[i][j][\'' + mat  + '\']')
 
-mlab_dict = {}
-mlab_dict['cmatrix'] = mlab_n;
-sio.savemat(outputBaseName + '_number_of_fibers.mat',mlab_dict)
+#----------------
+# save matrices in the requested file format
 
-mlab_dict['cmatrix'] = mlab_lm;
-sio.savemat(outputBaseName + '_fiber_length_mean.mat',mlab_dict)
+print 'Saving matrices as: *.' + options.extension + ' files'
 
-mlab_dict['cmatrix'] = mlab_ls;
-sio.savemat(outputBaseName + '_fiber_length_std.mat',mlab_dict)
+if options.extension == 'txt':
+  for mat in listkeys:
+    np.savetxt( options.base + '_' + mat + '.' + options.extension ,eval(mat),fmt=listformat[listkeys.index(mat)], delimiter='\t', newline='\n')
+elif options.extension == 'mat':
+  mlab_dict = {}
+  for mat in listkeys:
+    mlab_dict['cmatrix'] = eval(mat);
+    sio.savemat(options.base + '_' + mat + '.' + options.extension,mlab_dict)
+else:
+  print 'Invalid file extension: ' + options.extension
 
 print 'Done'
-     
-    
-    
-#    connectome_to_matlab(outputBaseName + '_number_of_fibers.mat', g, 'number_of_fibers')
- #   connectome_to_matlab(outputBaseName + '_fiber_length_mean.mat', g, 'fiber_length_mean')
-  #  connectome_to_matlab(outputBaseName + '_fiber_length_std.mat', g, 'fiber_length_std')
+
+#    connectome_to_matlab(options.base + '_number_of_fibers.mat', g, 'number_of_fibers')
+ #   connectome_to_matlab(options.base + '_fiber_length_mean.mat', g, 'fiber_length_mean')
+  #  connectome_to_matlab(options.base + '_fiber_length_std.mat', g, 'fiber_length_std')
