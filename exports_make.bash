bf71bd6c2804fe98558f4991131ab8dec8c01f1a
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2012-09-05 15:05:05 -0400

    Add another subnet to Waltham (WiFi)

--- exports_make.bash
+++ exports_make.bash
@@ -65,7 +65,7 @@ done
 G_STYLE=$(string_clean $G_STYLE)
 if [[ $G_STYLE != "Linux" && $G_STYLE != "Darwin" ]] ; then fatal args;   fi
 
-sitenum=20
+sitenum=21
 
 # Format: <label>;<netmask>
  NETMASK[0]="1 Autumn Street, 6th floor;10.17.24.0"
@@ -77,17 +77,18 @@ sitenum=20
  NETMASK[6]="Waltham (WL13W3 - subnet 1);10.64.4.0"
  NETMASK[7]="Waltham (WL13W3 - subnet 2);10.64.5.0"
  NETMASK[8]="Waltham (WL13W3 - subnet 3);10.64.84.0"
- NETMASK[9]="1 Autumn Street, TCHpeap subnet 1;10.23.50.0"
-NETMASK[10]="1 Autumn Street, TCHpeap subnet 2;10.23.128.0"
-NETMASK[11]="1 Autumn Street, TCHpeap subnet 3;10.23.129.0"
-NETMASK[12]="1 Autumn Street, TCHpeap subnet 4;10.23.130.0"
-NETMASK[13]="1 Autumn Street, TCHpeap subnet 5;10.23.131.0"
-NETMASK[14]="1 Autumn Street, TCHpeap subnet 6;10.23.132.0"
-NETMASK[15]="1 Autumn Street, TCHpeap subnet 7;10.23.133.0"
-NETMASK[16]="1 Autumn Street, TCHpeap subnet 8;10.23.134.0"
-NETMASK[17]="1 Autumn Street, TCHpeap subnet 9;10.23.135.0"
-NETMASK[18]="Main CHB Campus, 3D Lab;10.3.2.0"
-NETMASK[19]="Engels Lab;10.32.72.0"
+ NETMASK[9]="Waltham (Read   - subnet 4);10.65.130.0"
+NETMASK[10]="1 Autumn Street, TCHpeap subnet 1;10.23.50.0"
+NETMASK[11]="1 Autumn Street, TCHpeap subnet 2;10.23.128.0"
+NETMASK[12]="1 Autumn Street, TCHpeap subnet 3;10.23.129.0"
+NETMASK[13]="1 Autumn Street, TCHpeap subnet 4;10.23.130.0"
+NETMASK[14]="1 Autumn Street, TCHpeap subnet 5;10.23.131.0"
+NETMASK[15]="1 Autumn Street, TCHpeap subnet 6;10.23.132.0"
+NETMASK[16]="1 Autumn Street, TCHpeap subnet 7;10.23.133.0"
+NETMASK[17]="1 Autumn Street, TCHpeap subnet 8;10.23.134.0"
+NETMASK[18]="1 Autumn Street, TCHpeap subnet 9;10.23.135.0"
+NETMASK[19]="Main CHB Campus, 3D Lab;10.3.2.0"
+NETMASK[20]="Engels Lab;10.32.72.0"
 
 shift $(($OPTIND - 1))
 EXPORTLIST=$*
