7f44cd9841c3f36fcd3838033802ae3581c2a48d
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-11-28 16:03:45 -0500

    Modify mount-* scripts to use cifs instead of smbmount.

--- mount_longwood-3.0T.sh
+++ mount_longwood-3.0T.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo smbmount //10.3.1.214/MedCom /mnt/siemens_oc_longwood-3.0T -o uid=6244,gid=1102,user=meduser,password=meduser1
+sudo mount -t cifs //10.3.1.214/MedCom /mnt/siemens_oc_longwood-3.0T -o uid=6244,gid=1102,user=meduser,password=meduser1
