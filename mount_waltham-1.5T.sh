e705adca3e19600623dffb96bb01d31607469a1e
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-11-28 16:03:45 -0500

    Modify mount-* scripts to use cifs instead of smbmount.

--- mount_waltham-1.5T.sh
+++ mount_waltham-1.5T.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo smbmount //10.64.4.216/MedCom /mnt/siemens_oc_waltham-1.5T -o uid=6244,gid=1102,user=meduser,password=meduser1
+sudo mount -t cifs //10.64.4.216/MedCom /mnt/siemens_oc_waltham-1.5T -o uid=6244,gid=1102,user=meduser,password=meduser1
