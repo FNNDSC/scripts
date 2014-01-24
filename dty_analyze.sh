b82e30fe4075640de397a2ede108e511eb9da140
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2014-01-24 13:46:51 -0500

    Add "stages" design to dty_analyze.sh Add tautona tunnel to mcheck-ssh-dreev.bash

--- dty_analyze.sh
+++ dty_analyze.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+# "include" the set of common script functions
+source common.bash
+
+G_STAGES="12"
+DENSITYLIST="AreaDensity.txt ParticleDensity.txt"
+TOKEN="cloudCoreOverlap"
+OUTDIR="./"
+
+FILTER="le5"
+PREFIXLIST=""
+let b_prefixList=0
+
+
 G_SYNPOSIS="
 
   NAME
@@ -50,19 +64,28 @@ G_SYNPOSIS="
 	
 	Defaults to './'
 
+  STAGES
+
+	1 - summarize
+		Build the density table files by filtering the space
+		of outputs conforming to a filter.
+        2 - threshold
+		Parse the density table files and determine a new
+		threshold cut off to parse all density files.
+
+
   HISTORY
   
   	10-Jan-2014
 	o Initial design and coding.
 "
 
-DENSITYLIST="AreaDensity.txt ParticleDensity.txt"
-TOKEN="cloudCoreOverlap"
-OUTDIR="./"
+G_SELF=`basename $0`
+G_PID=$$
 
-FILTER="le5"
-PREFIXLIST=""
-let b_prefixList=0
+A_noOutRunDir="checking on output run directory"
+EM_noOutRunDir="I couldn't access the output run dir. Does it exist?"
+EC_noOutRootDir=54
 
 while getopts v:s:p:o: option ; do
         case "$option"
@@ -71,46 +94,96 @@ while getopts v:s:p:o: option ; do
 		p) PREFIXLIST=$OPTARG			
 		   b_prefixList=${#PREFIXLIST}	;;
                 s) FILTER=$OPTARG		;;
+                t) G_STAGES="$OPTARG"		b;;
                 v) let Gi_verbose=$OPTARG       ;;
                 \?) synopsis_show
                     exit 0;;
         esac
 done
 
+verbosity_check
+topDir=$(pwd)
+
+# Are we on a Mac with installed MacPorts?
+EXPR=expr
+fileExist_check /opt/local/bin/gexpr && EXPR=/opt/local/bin/gexpr
+
+echo "expr = $EXPR"
+exit 0
+
+printf "\n"
+cprint  "hostname"      "[ $(hostname) ]"
+
+G_LOGDIR=$OUTDIR
+lprint          "Checking on output root dir"
+dirExist_check ${G_LOGDIR} "not found - creating"  \
+              || mkdir -p ${G_LOGDIR}              \
+              || fatal noOutRootDir
+cd $OUTDIR >/dev/null
+G_LOGDIR=$(pwd)
+cd $topDir >/dev/null
+
 if (( b_prefixList )) ; then
 	PREFIXLIST=$(echo "$PREFIXLIST" | tr "," " ")
 else
 	PREFIXLIST="*"
 fi
-	
-ALLHITS=""
-b_removeResultFiles=0
-for PREFIX in $PREFIXLIST; do 
-	if (( b_prefixList )) ; then
-		PREFIXHITS=$(find . -iname "*$PREFIX*$FILTER*")
-	else
-		PREFIXHITS=$(find . -iname "*$FILTER*")
-	fi
-	b_HITS=$(echo "$HITS" | wc -l)
-	b_removeResultFiles=$(( b_HITS || b_removeResultFiles))
-	ALLHITS=$(printf "%s\n%s" "$ALLHITS" "$PREFIXHITS")
-	if (( !b_prefixList )) ; then 
-		break
-	fi
+
+## Check which stages to process
+statusPrint     "Checking which stages to process"
+barr_stage=([0]=0 [1]=0)
+for i in $(seq 1 2) ; do
+        b_test=$(expr index $G_STAGES "$i")
+        if (( b_test )) ; then b_flag="1" ; else b_flag="0" ; fi
+        barr_stage[$i]=$b_flag
 done
-if [[ b_removeResultFiles ]] ; then
-	rm -f $DENSITYLIST
+ret_check $?
+
+STAMPLOG=${G_LOGDIR}/${G_SELF}.log
+stage_stamp "Init | ($(pwd)) $G_SELF $*" $STAMPLOG
+
+STAGENUM="1-dty_analyze"
+STAGEPROC="summaryTables"
+STAGE=${STAGENUM}-${STAGEPROC}
+STAGE1RELDIR=${G_OUTRUNDIR}/${STAGE}
+STAGE1FULLDIR=${OUTDIR}/${STAGE}
+statusPrint     "Checking stage 1 output dir"
+dirExist_check ${OUTDIR}/${STAGE} "not found - creating"        \
+            || mkdir -p ${OUTDIR}/${STAGE}                      \
+            || fatal noOutRunDir
+if (( ${barr_stage[1]} )) ; then
+	statusPrint "$(date) | Processing Stage $STAGENUM" "\n"
+	ALLHITS=""
+	b_removeResultFiles=0
+	for PREFIX in $PREFIXLIST; do 
+		if (( b_prefixList )) ; then
+			PREFIXHITS=$(find . -iname "*$PREFIX*$FILTER*")
+		else
+			PREFIXHITS=$(find . -iname "*$FILTER*")
+		fi
+		b_HITS=$(echo "$HITS" | wc -l)
+		b_removeResultFiles=$(( b_HITS || b_removeResultFiles))
+		ALLHITS=$(printf "%s\n%s" "$ALLHITS" "$PREFIXHITS")
+		if (( !b_prefixList )) ; then 
+			break
+		fi
+	done
+	if [[ b_removeResultFiles ]] ; then
+		rm -f $DENSITYLIST
+	fi
+
+	for HIT in $ALLHITS ; do
+		DIR=$(echo $HIT   | gxargs -i% echo "dirname %"   | sh)
+		FILE=$(echo $HIT  | gxargs -i% echo "basename %"  | sh)
+		for DTY in $DENSITYLIST ; do
+			STEM=$(echo $FILE | sed 's/\(.*\)'${TOKEN}'\(.*\)/\1'${TOKEN}${DTY}'/')
+			if (( Gi_verbose )); then
+				printf "%s    %s  %s \n" $DIR $FILE $STEM
+			fi
+			CONTENTS=$(cat $DIR/$STEM)
+			echo -e "$CONTENTS\t$DIR/$STEM" >> $DTY
+		done
+	done
+
 fi
 
-for HIT in $ALLHITS ; do
-	DIR=$(echo $HIT   | gxargs -i% echo "dirname %"   | sh)
-	FILE=$(echo $HIT  | gxargs -i% echo "basename %"  | sh)
-	for DTY in $DENSITYLIST ; do
-		STEM=$(echo $FILE | sed 's/\(.*\)'${TOKEN}'\(.*\)/\1'${TOKEN}${DTY}'/')
-		if (( Gi_verbose )); then
-			printf "%s    %s  %s \n" $DIR $FILE $STEM
-		fi
-		CONTENTS=$(cat $DIR/$STEM)
-		echo -e "$CONTENTS\t$DIR/$STEM" >> $DTY
-	done
-done
