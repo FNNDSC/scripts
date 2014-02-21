825d78038c479a0b4a6f53744f1b47810088e950
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2014-02-21 12:29:59 -0500

    Add forward connection MGH->BCH ChRIS on 8443.

--- mcheck-ssh-kaos.bash
+++ mcheck-ssh-kaos.bash
@@ -105,7 +105,7 @@ for file in $REQUIREDFILES ; do
         file_checkOnPath $file >/dev/null || fatal fileCheck
 done
 
-targetList=24
+targetList=25
  TARGET_CHECK[0]="tunnel.bash --reverse --from ch137123@${DREEV}:10301 --to localhost:10401 --isRunning"
  TARGETACTION[0]="tunnel.bash --reverse --from ch137123@${DREEV}:10301 --to localhost:10401"
  TARGET_CHECK[1]="tunnel.bash --forward --from 4212 --via ch137123@${DREEV} --to localhost:4212 --isRunning"
@@ -154,7 +154,8 @@ TARGET_CHECK[22]="tunnel.bash --forward --from 2137 --via ch137123@${DREEV} --to
 TARGETACTION[22]="tunnel.bash --forward --from 2137 --via ch137123@${DREEV} --to localhost:2137"
 TARGET_CHECK[23]="tunnel.bash --forward --from 3228 --via ch137123@${DREEV} --to localhost:3228 --isRunning"
 TARGETACTION[23]="tunnel.bash --forward --from 3228 --via ch137123@${DREEV} --to localhost:3228"
-
+TARGET_CHECK[24]="tunnel.bash --forward --from 8443 --via ch137123@${DREEV} --to localhost:8443 --isRunning"
+TARGETACTION[24]="tunnel.bash --forward --from 8443 --via ch137123@${DREEV} --to localhost:8443"
 
 while getopts hv: option ; do
         case "$option"
