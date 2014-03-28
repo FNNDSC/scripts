b462b70eaf49cf5744edc06fd99e1732185f71b1
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2014-03-28 08:47:03 -0400

    Add reverse tunnel to 'heisenberg' on 7772.

--- mcheck-ssh-kaos.bash
+++ mcheck-ssh-kaos.bash
@@ -105,7 +105,7 @@ for file in $REQUIREDFILES ; do
         file_checkOnPath $file >/dev/null || fatal fileCheck
 done
 
-targetList=26
+targetList=27
  TARGET_CHECK[0]="tunnel.bash --reverse --from ch137123@${DREEV}:10301 --to localhost:10401 --isRunning"
  TARGETACTION[0]="tunnel.bash --reverse --from ch137123@${DREEV}:10301 --to localhost:10401"
  TARGET_CHECK[1]="tunnel.bash --forward --from 4212 --via ch137123@${DREEV} --to localhost:4212 --isRunning"
@@ -158,6 +158,8 @@ TARGET_CHECK[24]="tunnel.bash --forward --from 8443 --via ch137123@${DREEV} --to
 TARGETACTION[24]="tunnel.bash --forward --from 8443 --via ch137123@${DREEV} --to localhost:8443"
 TARGET_CHECK[25]="tunnel.bash --forward --from 3142 --via ch137123@${DREEV} --to localhost:3142 --isRunning"
 TARGETACTION[25]="tunnel.bash --forward --from 3142 --via ch137123@${DREEV} --to localhost:3142"
+TARGET_CHECK[26]="tunnel.bash --reverse --from ch137123@${DREEV}:7772 --to heisenberg:22 --isRunning"
+TARGETACTION[26]="tunnel.bash --reverse --from ch137123@${DREEV}:7772 --to heisenberg:22"
 
 while getopts hv: option ; do
         case "$option"
