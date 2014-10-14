180ce11ba4063258763467f87ee20acdb291fb86
Nicolas Rannou <huko.rannou@gmail.com>
2014-10-14 03:23:44 -0400

    mount C drive of the longwood 3T scanner in addition to MedCom

--- mount_longwood-3.0T.sh
+++ mount_longwood-3.0T.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo mount -t cifs //10.3.1.214/MedCom /mnt/siemens_oc_longwood-3.0T -o uid=6244,gid=1102,user=meduser,password=meduser1
+sudo mount -t cifs //10.3.1.214/Disk_C /mnt/siemens_oc_longwood-3.0T-Disk_C -o uid=6244,gid=1102,user=meduser,password=meduser1
