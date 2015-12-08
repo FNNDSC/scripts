0e0a8dc49ad487cc160e06a96ff1a5d5f3e82fc7
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2015-12-08 08:16:26 -0500

    Add new wifi subnet.

--- exports_make.bash
+++ exports_make.bash
@@ -79,7 +79,7 @@ if [[ $G_STYLE != "Linux" && $G_STYLE != "Darwin" ]] ; then fatal args;   fi
 if (( Gb_siteNum )) ; then
     sitenum=$G_siteNum
 else
-    sitenum=44
+    sitenum=45
 fi
 
 # Format: <label>;<netmask>
@@ -127,6 +127,7 @@ NETMASK[40]="Autumn Street, 6th Floor;10.23.48.0"
 NETMASK[41]="Christos/etc;10.5.14.0"
 NETMASK[42]="Christos/etc;10.5.13.0"
 NETMASK[43]="1 Autumn Street, TCHpeap subnet 18;10.23.53.0"
+NETMASK[44]="1 Autumn Street, TCHpeap subnet 18;10.23.54.0"
 
 shift $(($OPTIND - 1))
 EXPORTLIST=$*
