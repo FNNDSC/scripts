b1bb62642661a8775f73f19a67083a5bc533f3a5
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2016-03-19 12:19:34 -0400

    Fix H1.

--- mcheck-ssh-toBCHvia_door.bash
+++ mcheck-ssh-toBCHvia_door.bash
@@ -100,7 +100,7 @@ PRETORIA=pretoria.tch.harvard.edu
 GATE=gate.nmr.mgh.harvard.edu
 DOOR=door.nmr.mgh.harvard.edu
 #DOOR=gate.nmr.mgh.harvard.edu
-H1=108.20.43.104
+H1=173.48.111.167
 
 verbosity_check
 REQUIREDFILES="common.bash tunnel.bash pgrep"
