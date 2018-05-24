0be033a2a03f5519bbb49172735090eebd250932
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-05-24 14:12:02 -0400

    Add LM wifi subnet.

--- exports_make.bash
+++ exports_make.bash
@@ -79,7 +79,7 @@ if [[ $G_STYLE != "Linux" && $G_STYLE != "Darwin" ]] ; then fatal args;   fi
 if (( Gb_siteNum )) ; then
     sitenum=$G_siteNum
 else
-    sitenum=60
+    sitenum=61
 fi
 
 # Format: <label>;<netmask>
@@ -143,6 +143,7 @@ NETMASK[56]="7 Landmark, subnet 6;10.72.83.0"
 NETMASK[57]="7 Landmark, subnet 6;10.72.84.0"
 NETMASK[58]="7 Landmark, subnet 7;10.72.85.0"
 NETMASK[59]="7 Landmark, subnet 8;10.72.86.0"
+NETMASK[60]="7 Landmark, wifi subnet 1;10.23.58.0"
 
 shift $(($OPTIND - 1))
 EXPORTLIST=$*
